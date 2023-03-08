0108f8cb85c62d445922debb9f767d6b59b844a9
AbdulAhad2659 <102375793+AbdulAhad2659@users.noreply.github.com>
2023-03-08 13:48:44 +0500

    Set up camera for attendance

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+import cv2
 
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
 
-if __name__ == '__main__':
-    print_hi('Pycharm')
+while True:
+    success, img = cap.read()
+    cv2.imshow("Face Attendance", img)
+    cv2.waitKey(1)
+
