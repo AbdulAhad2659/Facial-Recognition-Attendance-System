c6e29aa05c1c5d657af7563188f0ca8a05bb9c59
AbdulAhad2659 <102375793+AbdulAhad2659@users.noreply.github.com>
2023-03-09 13:33:41 +0500

    Generate encodings for images

--- EncodeGenerator.py
+++ EncodeGenerator.py
@@ -1,4 +1,32 @@
 import cv2
 import face_recognition
 import pickle
+import os
+
+folderPath = "Images"
+pathList = os.listdir(folderPath)
+imgList = []
+studentIds = []
+
+for path in pathList:
+    imgList.append(cv2.imread(os.path.join(folderPath, path)))
+    studentIds.append(os.path.splitext(path)[0])
+
+
+def findEncodings(imagesList):
+    encodeList = []
+
+    for img in imagesList:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+
+    return encodeList
+
+
+print("Encoding Started...")
+
+encodeListKnown = findEncodings(imgList)
+
+print("Encoding Finished.")
 
