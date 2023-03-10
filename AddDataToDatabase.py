6dfae1470fd4d13634d3613a44ec72fa969a13e4
AbdulAhad2659 <102375793+AbdulAhad2659@users.noreply.github.com>
2023-03-10 11:54:32 +0500

    Added 3 data examples to firebase

--- AddDataToDatabase.py
+++ AddDataToDatabase.py
@@ -19,7 +19,30 @@ data = {
             "Standing: ": "G",
             "Year: ": 4,
             "Last Attendance: ": "2023-03-10 11:36:00"
+        },
+
+    "852741":
+        {
+            "Name: ": "Lewis Hamilton",
+            "Major: ": "Racer",
+            "Starting Year: ": 2007,
+            "Total Attendance: ": 8,
+            "Standing: ": "A",
+            "Year: ": 16,
+            "Last Attendance: ": "2023-03-10 11:39:11"
+        },
+
+    "963852":
+        {
+            "Name: ": "Abdul Ahad",
+            "Major: ": "AI",
+            "Starting Year: ": 2018,
+            "Total Attendance: ": 9,
+            "Standing: ": "F",
+            "Year: ": 3,
+            "Last Attendance: ": "2023-03-10 11:49:18"
         }
 }
+
 for key, value in data.items():
     ref.child(key).set(value)
