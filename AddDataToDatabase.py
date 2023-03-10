dee850459337523ba9dab18290b0b8877f0b0fd3
AbdulAhad2659 <102375793+AbdulAhad2659@users.noreply.github.com>
2023-03-10 11:45:27 +0500

    Added some data to firebase storage

--- AddDataToDatabase.py
+++ AddDataToDatabase.py
@@ -1,5 +1,25 @@
 import firebase_admin
 from firebase_admin import credentials
+from firebase_admin import db
 
-cred = credentials.Certificate("path/to/serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://faceattendancerealtime-1e56a-default-rtdb.firebaseio.com/'
+})
+
+ref = db.reference('Students')
+
+data = {
+    "321654":
+        {
+            "Name: ": "Katy Perry",
+            "Major: ": "Singer",
+            "Starting Year: ": 2005,
+            "Total Attendance: ": 6,
+            "Standing: ": "G",
+            "Year: ": 4,
+            "Last Attendance: ": "2023-03-10 11:36:00"
+        }
+}
+for key, value in data.items():
+    ref.child(key).set(value)
